77fa8340341a3c37cb0fa3f13942bd1dfee91e6d
Stephan Oehlert <stephan.oehlert@cornelsen.de>
2016-07-27 17:32:20 +0200

    Initial commit.

---
+++ Web/Controllers/AController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/some")]
+    public class AController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable Get()
+        {
+            return ThrowFromIterator();
+        }
+
+        public IEnumerable ThrowFromIterator()
+        {
+            throw new ArgumentException();
+            yield break;
+        }
+    }
+}
---
+++ Web/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Web
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseMvc();
+        }
+    }
+}
